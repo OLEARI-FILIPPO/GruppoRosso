a38d3521a3e041204c7ac0e2993b328725a9a29e
Youness <youness.moussadek98@gmail.com>
2021-10-15 13:58:50 +0200

    Aggiunto le classi

---
+++ ProgettoGarage/Garage/Classi/Parcheggio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Classi
+{
+    class Parcheggio
+    {
+        private string TargaMacchina { get; set; }
+
+        private bool StatoParcheggio { get; set; }
+
+
+    }
+}
---
+++ ProgettoGarage/Garage/Classi/Veicolo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Classi
+{
+    class Veicolo
+    {
+        private Persona P { get; set; }
+
+        private string _targa;
+        public string Targa
+        {
+            get
+            {
+                return _targa;
+            }
+            set
+            {
+                _targa = value;
+            }
+        }
+
+        public Veicolo(string Targa)
+        {
+            this.Targa = Targa;
+        }
+
+        private string _colore;
+        public string Colore
+        {
+            get
+            {
+                return _colore;
+            }
+            set
+            {
+                _colore = value;
+            }
+        }
+
+        private string _marca;
+        public string Marca
+        {
+            get
+            {
+                return _marca;
+            }
+            set
+            {
+                _marca = value;
+            }
+        }
+
+        private string _modello;
+        public string Modello
+        {
+            get
+            {
+                return _modello;
+            }
+            set
+            {
+                _modello = value;
+            }
+        }
+
+        public void Descrizione(string Colore, string Marca, string Modello)
+        {
+            this.Colore = Colore;
+            this.Marca = Marca;
+            this.Modello = Modello;
+        }
+
+    }
+}
