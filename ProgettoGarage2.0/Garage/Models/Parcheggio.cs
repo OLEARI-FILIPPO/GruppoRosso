22181489b34787341334c89ec1ccef72002dcc1c
Danishveer Singh <88554811+danishS17@users.noreply.github.com>
2021-10-17 19:44:43 +0200

    Caricato la cartella della versione 2.0

---
+++ ProgettoGarage2.0/Garage/Models/Parcheggio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Classi
+{
+    class Parcheggio
+    {
+        public string TargaMacchina { get; set; }
+       
+        public bool StatoParcheggio { get; set; }
+
+        public string row { get; set; }
+
+        public string col { get; set; }
+
+        public Parcheggio(string row, string col)
+        {
+            
+            StatoParcheggio = false;
+            this.row = row;
+            this.col = col;
+        }
+
+        public string Entra()
+        {
+
+            string parking;
+            parking = "P" + row + col;
+            // StatoParcheggio = true;
+
+            return parking;
+
+           
+            
+        }
+
+       
+    }
+}
