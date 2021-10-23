634972b1fb9f1194128e7c719cd3785d14dbeceb
Danishveer Singh <88554811+danishS17@users.noreply.github.com>
2021-10-23 13:48:42 +0200

    Aggiunto le classi restanti

---
+++ Progetto3.0/ProgettoGarageDatabase/GarageDatabase/Classi/Automobile.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageDatabase.Classi
+{
+    class Automobile
+    {
+        //Campi caratteristiche della classe Auomobile
+        private string _marca;
+        public string Marca
+        {
+            get
+            {
+                return _marca;
+            }
+            set
+            {
+                _marca = value;
+            }
+        }
+
+        private string _modello;
+        public string Modello
+        {
+            get
+            {
+                return _modello;
+            }
+            set
+            {
+                _modello = value;
+            }
+        }
+
+        private string _targa;
+        public string Targa
+        {
+            get
+            {
+                return _targa;
+            }
+            set
+            {
+                _targa = value;
+            }
+        }
+
+        //Campi  chiavi Id
+        private int _idAuto;
+        public int IdAuto
+        {
+            get
+            {
+                return _idAuto;
+            }
+            set
+            {
+                _idAuto = value;
+            }
+        }
+
+        private int _idPersona;
+        public int IdPersona
+        {
+            get
+            {
+                return _idPersona;
+            }
+            set
+            {
+                _idPersona = value;
+            }
+        }
+
+
+        //Costruttore Automobile
+        public Automobile(string Marca, string Modello, string Targa)
+        {
+            this.Modello = Modello;
+            this.Marca = Marca;
+            this.Targa = Targa;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Progetto3.0/ProgettoGarageDatabase/GarageDatabase/Classi/Storico.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageDatabase.Classi
+{
+    class Storico
+    {
+        //Campi  chiavi Id
+        private int _idStorico;
+        public int IdStorico
+        {
+            get
+            {
+                return _idStorico;
+            }
+            set
+            {
+                _idStorico = value;
+            }
+        }
+
+        private int _idAuto;
+        public int IdAuto
+        {
+            get
+            {
+                return _idAuto;
+            }
+            set
+            {
+                _idAuto = value;
+            }
+        }
+
+        private int _idparcheggio;
+        public int IdParcheggio
+        {
+            get
+            {
+                return _idparcheggio;
+            }
+            set
+            {
+                _idAuto = value;
+            }
+        }
+
+
+        //Campi caratteristiche della classe Storico
+        private DateTime _dataOrarioIngresso;
+        public DateTime DataOrarioIngresso
+        {
+            get
+            {
+                return _dataOrarioIngresso;
+            }
+            set
+            {
+                _dataOrarioIngresso = value;
+            }
+        }
+
+        private DateTime _dataOraUscita;
+        public DateTime DataOraUscita
+        {
+            get
+            {
+                return _dataOraUscita;
+            }
+            set
+            {
+                _dataOraUscita = value;
+            }
+        }
+
+        //Costruttore Storico
+        public Storico(DateTime DataOrarioIngresso, DateTime DataOraUscita)
+        {
+            this.DataOrarioIngresso = DataOrarioIngresso;
+            this.DataOraUscita = DataOraUscita;
+        }
+    }
+}
